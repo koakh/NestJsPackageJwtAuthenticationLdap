a281ea66cbe03f959350de7dd5eb639aa69cc7b2
Mario Monteiro <marioammonteiro@gmail.com>
2022-03-15 15:14:27 +0000

    done with searchUsersCn and events, but now must convert events to callbacks with argument types and responses

--- nestjs-package-jwt-authentication-ldap/src/common/interfaces/consumer-app-service.interface.ts
+++ nestjs-package-jwt-authentication-ldap/src/common/interfaces/consumer-app-service.interface.ts
@@ -5,6 +5,15 @@ export interface ConsumerAppService {
   licenseState: () => any;
   initRenewTokenSecrets(): JwtSecrets;
   getJwtSecrets: () => JwtSecrets;
-  singleSignOn: (req: any,res: any) => any;
-  chpasswd: (username: string,password: string) => any;
+  singleSignOn?: (req: any, res: any) => any;
+  changePassword?: (username: string, password: string) => any;
+  // events
+  onCreateUserRecord: () => void; //ok
+  onChangeUserRecord: () => void; //ok
+  onDeleteUserRecord: () => void; //ok
+  onAddOrDeleteUserToGroup: () => void; //ok
+  onChangeUserProfilePassword: () => void; //ok 
+  onUpdateDefaultGroup: () => void; //ok
+  onCreateGroupRecord: () => void; //ok
+  onDeleteGroupRecord: () => void; // ok onDeleteGroupRecord
 }
